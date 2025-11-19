8fc16dfb99c21558b22e7bc6106c75c02699cba7
JW Moon <moon910904@gmail.com>
2025-11-19 20:51:03 +0900

    chore: add FRONTEND_URL

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  });
   app.useGlobalPipes(new ZodValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
